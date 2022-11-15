dace9c0499fa2055a771a03a3146325f156bb16a
Denys-Fadieiev <dencheek0911@gmail.com>
2022-11-15 17:15:33 +0200

    add UserList component

--- src/components/UsersList/index.jsx
+++ src/components/UsersList/index.jsx
@@ -11,21 +11,39 @@
 // Interaction Tasks:
 // Добавте пользователям свойство likes, показывающее количество их лайков. Добавте  в верстку Пользователя кнопку, позволяющую лайкнуть пользователя. Лайкнуть его кнопкой можно только один раз
 
-import React from "react";
+import User from '../User';
+import React, { Component } from 'react';
 
-class User extends React.Component {
+class UserList extends Component {
+  state = {
+    users: [
+      { id: 1, name: 'John', surname: 'Snow', isLike: false },
+      { id: 2, name: 'Piter', surname: 'Parker', isLike: false },
+      { id: 3, name: 'Alex', surname: 'Tarasow', isLike: false },
+      { id: 4, name: 'liza', surname: 'Kirilenko', isLike: false },
+      { id: 5, name: 'Nataly', surname: 'Portman', isLike: false },
+    ],
+  }
+
   render() {
-    const {id, name, surname, isLike} = this.props;
-    return(
-      <>
-        <p>
-          <i>User ID: {id}</i>
-        </p>
-        <p>{name} {surname}</p>
-        <p>Likes: {isLike ? 1 : 0}</p>
-      </>
-    )
+    const usersListRender = this.state.users.map(({ id, name, surname, isLike}) => (
+      <li key={id}>
+        <User
+        id={id}
+        name={name}
+        surname={surname}
+        isLike={isLike}
+        />
+      </li>
+    ))
+    return (
+      <article>
+        <ul><b>Users list:</b> 
+          {usersListRender}
+        </ul>
+      </article>
+    );
   }
 }
 
-export default User;
+export default UserList;
